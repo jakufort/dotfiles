8064bdf5933ab6cb43db2aa3270610cd739df245
Jakub Fortunka <jakufort@gmail.com>
2025-04-13 19:26:18 +0200

    finishing nvim config

--- .config/nvim/lua/custom/plugins/autocompletion.lua
+++ .config/nvim/lua/custom/plugins/autocompletion.lua
@@ -58,10 +58,14 @@ return {
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'super-tab',
+        preset = 'default',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<Esc>'] = { 'hide', 'fallback' },
+        ['<PageUp>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<PageDown>'] = { 'scroll_documentation_down', 'fallback' },
       },
 
       appearance = {
