e232aaf24ab2d2c0e617a0e668fe7158dd3d81df
Jakub Fortunka <jakufort@gmail.com>
2025-04-28 01:14:05 +0200

    sway gammastep

---
+++ .config/sway/scripts/gammastep-toggle.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+TOGGLE=$HOME/.gammastep_toggle
+
+while getopts t: flag
+do
+    case "${flag}" in
+        t) temp=${OPTARG};;
+    esac
+done
+
+if [ ! -e $TOGGLE ]; then
+    touch $TOGGLE
+    gammastep -P -O $temp
+else
+    rm $TOGGLE
+    pkill gammastep
+fi
