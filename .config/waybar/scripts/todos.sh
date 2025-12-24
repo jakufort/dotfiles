9186d1f289030852d41cc75a7cc1f5463528176d
Jakub Fortunka <jakufort@gmail.com>
2025-12-24 20:09:59 +0100

    waybar: making todos to display in the same way as different components

--- .config/waybar/scripts/todos.sh
+++ .config/waybar/scripts/todos.sh
@@ -2,6 +2,11 @@
 
 tasks=$(task status:pending export | jq -r '.[] | .description')
 
+if [ -z "$(printf '%s' "$tasks" | tr -d '[:space:]')" ]; then
+    echo '{"text":""}'
+    return 0
+fi
+
 escaped_new_lines=$(echo "$tasks" | sed -z 's/\n/\\n/g')
 
 echo '{"text": "todos", "tooltip": "'$escaped_new_lines'"}'
